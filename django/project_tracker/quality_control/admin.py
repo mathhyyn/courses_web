787b81d41df368c13407f7f45db0be4f9c369ccb
Анна Терентьева <annyshka20072002@gmail.com>
2024-04-30 18:50:39 +0300

    hw2 пользовательское действие

--- django/project_tracker/quality_control/admin.py
+++ django/project_tracker/quality_control/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import BugReport, FeatureRequest
+from django.contrib.admin.actions import action
+
+
+@action(description='Set status on Complete of selected bugs')
+def mark_completed(modeladmin, request, queryset):
+    queryset.update(status='Completed')
 
 
 @admin.register(BugReport)
@@ -19,6 +25,7 @@ class BugReportAdmin(admin.ModelAdmin):
             'fields': ('project', 'task', 'status', 'priority')
         })
     )
+    actions = [mark_completed]
 
 
 @admin.register(FeatureRequest)
